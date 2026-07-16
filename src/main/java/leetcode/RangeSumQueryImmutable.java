dd816ca0639cd512af277105b705b529d9a662a5
iscdr <iscdr10@gmail.com>
2026-07-17 01:52:15 +0300

    Range Sum Query - Immutable

---
+++ src/main/java/leetcode/RangeSumQueryImmutable.java
@@ -0,0 +1,16 @@
+package leetcode;
+
+public class RangeSumQueryImmutable {
+    public RangeSumQueryImmutable(int[] nums) {
+        prefix = new int[nums.length + 1];
+        for (int i = 0; i < nums.length; ++i)
+            prefix[i + 1] = nums[i] + prefix[i];
+
+    }
+
+    public int sumRange(int left, int right) {
+        return prefix[right + 1] - prefix[left];
+
+    }
+    private int[] prefix;
+}
