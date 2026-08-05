bec774cb618cbb8ccc6022890854f933510fb890
iscdr <iscdr10@gmail.com>
2026-08-05 17:49:08 +0300

    Verify Preorder Serialization of a Binary Tree

---
+++ src/main/java/leetcode/VerifyPreorderSerializationofaBinaryTree.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+public class VerifyPreorderSerializationofaBinaryTree {
+    public boolean isValidSerialization(String preorder) {
+        int degree = 1;
+
+        for (final String node : preorder.split(",")) {
+            if (--degree < 0)
+                return false;
+            if (!node.equals("#"))
+                degree += 2;
+        }
+
+        return degree == 0;
+
+    }
+}
