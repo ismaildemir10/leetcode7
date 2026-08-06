b5896ccd51f0607ecfb1feeb1c8eb60067862da0
iscdr <iscdr10@gmail.com>
2026-08-07 02:06:11 +0300

    Increasing Triplet Subsequence

---
+++ src/main/java/leetcode/IncreasingTripletSubsequence.java
@@ -0,0 +1,19 @@
+package leetcode;
+
+public class IncreasingTripletSubsequence {
+    public boolean increasingTriplet(int[] nums) {
+        int first = Integer.MAX_VALUE;
+        int second = Integer.MAX_VALUE;
+
+        for (final int num : nums)
+            if (num <= first)
+                first = num;
+            else if (num <= second)
+                second = num;
+            else
+                return true;
+
+        return false;
+
+    }
+}
